38977b5c6df402993c7f7167a6a28bc9039045df
Roy F Guzman Rodriguez <royguzrod@gmail.com>
2020-03-24 00:50:27 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -30,6 +30,17 @@ class App < Sinatra::Base
   end
   
   get '/:operation/:number1/:number2' do 
-    
+    @number_1 = params[:number1].to_i
+    @number_2 = params[:number2].to_i
+    if params[:operation] == "add"
+      "#{@number_1+@number_2}"
+    elsif params[:operation] == "subtract"
+      "#{@number_1-@number_2}"
+    elsif params[:operation] == "multiply"
+      "#{@number_1*@number_2}"
+    elsif params[:operation] == "divide"
+      "#{@number_1/@number_2}"
+    end
+  end
   
 end
