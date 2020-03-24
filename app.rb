9a387120ce54566176a33f41c5daf7f5d9a4137c
Roy F Guzman Rodriguez <royguzrod@gmail.com>
2020-03-24 00:49:00 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -25,7 +25,9 @@ class App < Sinatra::Base
     "#{@phrases}"
   end
   
-  get '/say/:word1/:word2/:word3/:word4/:word5'
+  get '/say/:word1/:word2/:word3/:word4/:word5' do
+    "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}."
+  end
     
   end
   
